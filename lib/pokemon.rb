eac7dc30a0222c582531fa28de2ffa8674dba370
German Cruz <cruzgerman216@yahoo.com>
2020-04-25 16:35:39 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -2,10 +2,10 @@ class Pokemon
   attr_accessor :id, :name, :type, :db
 
   def initialize(id=nil, name, type, db)
-    @id = id 
-    @name = name 
+    @id = id
+    @name = name
     @type = type
     @db = db
   end
-  
+
 end
